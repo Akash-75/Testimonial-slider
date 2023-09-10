c72568e12ea4e9beaae29b1f091d5ed2ed1dce94
Akash-75 <akashpandey53358@gmail.com>
2023-09-10 09:42:06 +0530

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Testimonial from "./Components/Testimonial";
+import reviews from "./data";
+
+const App = () => {
+  return (
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+
+      <div className="text-center">
+
+        <h className="text-4xl font-bold">Our Testimonial</h>
+
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+
+        <Testimonial reviews = {reviews}/>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
